10ae32286ea60c267ac5bb7523376d17883a6e46
vyankatesh612 <vyankatesh612@gmail.com>
2020-03-05 19:24:55 +0530

    ADDED : First comutation

--- Desktop/Arithmatic computation/Arithmatic.sh
+++ Desktop/Arithmatic computation/Arithmatic.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -x
  echo "Welcome to Arithmatic computation"
 
-read -p "Enter first number :" first_num
-read -p "Enter second number :" second_num
-read -p "Enter third number :" third_num
+ read -p "Enter first number :" first_num
+ read -p "Enter second number :" second_num
+ read -p "Enter third number :" third_num
 
+ result=$((first_num+second_num*third_num))
