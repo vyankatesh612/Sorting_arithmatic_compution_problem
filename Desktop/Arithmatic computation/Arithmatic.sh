f6d65e6c3cc4255ebc7f163afb3b5f39fb4e79ce
vyankatesh612 <vyankatesh612@gmail.com>
2020-03-06 13:06:52 +0530

    ADDED : eighth computation

--- Desktop/Arithmatic computation/Arithmatic.sh
+++ Desktop/Arithmatic computation/Arithmatic.sh
@@ -1,15 +1,34 @@
 #!/bin/bash -x
+ 
+ function descendingsort()
+	{
+ 		for ((i=0;i<10;i++))
+ 		do
+   		for ((j=0;j<10;j++))
+   		do
+     			if [ ${array[i]%.*} -gt ${array[j]%.*} ]
+     			then
+      			temp=${array[i]}
+       			array[i]=${array[j]}
+       			array[j]=$temp
+     			fi
+   		done
+ 		done
+    echo ${array[@]}
+   }
+
  declare -A dictionary
  declare -a array
+
  echo "Welcome to Arithmatic computation"
 
  read -p "Enter first number :" first_num
  read -p "Enter second number :" second_num
  read -p "Enter third number :" third_num
  
- result1=$((first-num+second_num*third_num))
- result2=$((first-num*second_num+third_num))
- result3=$((third_num+first_num/second_num))
+ result1=$((first_num+second_num*third_num))
+ result2=$((first_num*second_num+third_num))
+ result3=$( echo "scale=2; $third_num+$first_num/$second_num" | bc )
  result4=$((first_num%second_num+third_num))
 
  dictionary[1]=$result1
@@ -22,3 +41,5 @@
    array[$i]=${dictionary[${i}]}
  done
  
+ descendingsort
+
